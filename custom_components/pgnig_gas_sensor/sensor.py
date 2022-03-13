74616be4f0220fed6ead6e83d784920bc07502ae
pawel.hulek <pawel.hulek@ocado.com>
2022-03-13 20:46:08 +0100

    add new attributes, new latest read algorithm, readme updated

--- custom_components/pgnig_gas_sensor/sensor.py
+++ custom_components/pgnig_gas_sensor/sensor.py
@@ -1,5 +1,6 @@
 """Platform for sensor integration."""
 from __future__ import annotations
+import logging
 
 import string
 from datetime import timedelta
@@ -14,6 +15,7 @@ from homeassistant.helpers.entity_platform import AddEntitiesCallback
 from homeassistant.helpers.typing import ConfigType, DiscoveryInfoType
 from homeassistant.util import Throttle
 
+_LOGGER = logging.getLogger(__name__)
 PLATFORM_SCHEMA = PLATFORM_SCHEMA.extend({
     vol.Required(CONF_USERNAME): cv.string,
     vol.Required(CONF_PASSWORD): cv.string,
@@ -21,7 +23,7 @@ PLATFORM_SCHEMA = PLATFORM_SCHEMA.extend({
 
 login_url = "https://ebok.pgnig.pl/auth/login?api-version=3.0"
 url_devices_list = "https://ebok.pgnig.pl/crm/get-ppg-list?api-version=3.0"
-data_url = "https://ebok.pgnig.pl/crm/get-all-ppg-readings-for-meter?pageSize=1&pageNumber=1&api-version=3.0&idPpg="
+data_url = "https://ebok.pgnig.pl/crm/get-all-ppg-readings-for-meter?pageSize=10&pageNumber=1&api-version=3.0&idPpg="
 
 headers = {
     'Content-Type': 'application/json',
@@ -75,7 +77,8 @@ class PgnigSensor(SensorEntity):
         self.password = config.get(CONF_PASSWORD)
         self.meter_id = meter_id
         self.entity_name = "PGNIG Gas Sensor " + meter_id
-        self.update = Throttle(timedelta(hours=24))(self._update)
+        self.update = Throttle(timedelta(hours=8))(self._update)
+        self._update()
 
     @property
     def unique_id(self) -> str | None:
@@ -90,13 +93,22 @@ class PgnigSensor(SensorEntity):
         """Return the state of the sensor."""
         return self._state
 
+    @property
+    def extra_state_attributes(self):
+        attrs = dict()
+        attrs["wear"] = self.wear
+        attrs["wear_unit_of_measurment"] = VOLUME_CUBIC_METERS
+        return attrs
+
     def _update(self) -> None:
         """Fetch new state data for the sensor.
         This is the only method that should fetch new data for Home Assistant.
         """
-        self._state = self.getData()
+        data = self.fetchReadingForMeter()
+        self._state = data.get("Value")
+        self.wear = data.get('Wear')
 
-    def getData(self) -> object:
+    def fetchReadingForMeter(self):
         token = login(self.username, self.password)
 
         response = requests.get(data_url + self.meter_id, headers={
@@ -104,4 +116,4 @@ class PgnigSensor(SensorEntity):
             'Accept': 'application/json',
             'AuthToken': token
         })
-        return response.json().get('MeterReadings')[0].get('Value')
+        return max(response.json().get('MeterReadings'), key=lambda z: z.get('Value'))
