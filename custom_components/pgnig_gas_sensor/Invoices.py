38060ef9f3892a78ee418882f81019c52bf2ed4e
pawelhulek <pawelhulek@gmail.com>
2022-04-23 22:34:29 +0200

    8 niezapłacone faktury (#11)
    * unpaid invoices
    
    Signed-off-by: pawel.hulek <pawelhulek@gmail.com>
    
    * bump version
    
    Signed-off-by: pawel.hulek <pawelhulek@gmail.com>
    
    * add Invoices to UI config
    
    Signed-off-by: pawel.hulek <pawelhulek@gmail.com>
    
    Co-authored-by: pawel.hulek <pawelhulek@gmail.com>

--- custom_components/pgnig_gas_sensor/Invoices.py
+++ custom_components/pgnig_gas_sensor/Invoices.py
@@ -7,7 +7,6 @@ T = TypeVar("T")
 
 
 def from_str(x: Any) -> str:
-    assert isinstance(x, str)
     return x
 
 
@@ -16,37 +15,30 @@ def from_datetime(x: Any) -> datetime:
 
 
 def from_float(x: Any) -> float:
-    assert isinstance(x, (float, int)) and not isinstance(x, bool)
     return float(x)
 
 
 def from_int(x: Any) -> int:
-    assert isinstance(x, int) and not isinstance(x, bool)
     return x
 
 
 def from_bool(x: Any) -> bool:
-    assert isinstance(x, bool)
     return x
 
 
 def from_none(x: Any) -> Any:
-    assert x is None
     return x
 
 
 def to_float(x: Any) -> float:
-    assert isinstance(x, float)
     return x
 
 
 def from_list(f: Callable[[Any], T], x: Any) -> List[T]:
-    assert isinstance(x, list)
     return [f(y) for y in x]
 
 
 def to_class(c: Type[T], x: Any) -> dict:
-    assert isinstance(x, c)
     return cast(Any, x).to_dict()
 
 
