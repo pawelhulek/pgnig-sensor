d952951f33a4b6d04129354b6cf24a9e520c4024
Mateusz Jaworski <mateusz@dictumsoft.eu>
2026-02-10 16:45:26 +0100

    Zmiana core logowania na OrlenID
    W związku z migracją spółek dodałem obsługe OrlenID

--- custom_components/pgnig_gas_sensor/PgnigApi.py
+++ custom_components/pgnig_gas_sensor/PgnigApi.py
@@ -1,6 +1,7 @@
 import string
 
 import requests
+import re
 
 from .Invoices import invoices_from_dict, Invoices
 from .PgpList import (PpgList, ppg_list_from_dict)
@@ -44,10 +45,39 @@ class PgnigApi:
         }).json())
 
     def login(self) -> string:
-        payload = {"identificator": self.username,
-                   "accessPin": self.password,
-                   "rememberLogin": "false",
-                   "DeviceId": "123",  # TODO randomize it
-                   "DeviceName": "Home Assistant: 99.9.999.99<br>",
-                   "DeviceType": "Web"}
-        return requests.request('POST', login_url, headers=headers, json=payload).json().get('Token')
+        init_url = 'https://ebok.myorlen.pl/auth/oid/init-login?api-version=3.0'
+
+        session = requests.Session()
+        headers = {
+            'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/144.0.0.0 Safari/537.36',
+            'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8'
+        }
+        init_data = {
+            "DeviceId": "a908313085dd4f16deaa4c15897e755e",
+            "DeviceType": "Web",
+            "DeviceName": "HomeAssistant wersja: 0.1",
+            "LightweightRedirectUrl": "https://ebok.myorlen.pl/?show=modal",
+            "FinalizeRegistrationRedirectUrl": "https://ebok.myorlen.pl/aktywuj-oid/"
+        }
+
+        response_init = session.post(init_url, json=init_data, headers={'Content-Type': 'application/json'})
+        redirect_url = response_init.json().get('RedirectUrl')
+        response_page = session.get(redirect_url, headers=headers)
+        match = re.search(r'action="([^"]+)"', response_page.text)
+
+        if match:
+            post_url = match.group(1).replace('&amp;', '&')
+            payload = {
+                'username': self.username,
+                'password': self.password,
+                'credentialId': ''
+            }
+            final_response = session.post(post_url, data=payload, headers=headers)
+
+            if "https://ebok.myorlen.pl/home" in final_response.url:
+                auth_token_url = f'https://ebok.myorlen.pl/auth/get-auth-token?deviceId={init_data["DeviceId"]}&api-version=3.0'
+                headers.update({'Referer': 'https://ebok.myorlen.pl/'})
+                res_auth = session.get(auth_token_url, headers=headers)
+                if res_auth.status_code == 200:
+                    return res_auth.json().get('Token')
+        return ""
